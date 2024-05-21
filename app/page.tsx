c8454d5ee2582210b4e02538e85b40a1927d7f7b
Nathan Beck <njosefbeck@gmail.com>
2024-05-21 18:53:18 -0500

    Add initial homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
+import Image from "next/image";
+import gypsyImg from './jobs/sprites/gypsy.png';
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      Hello!
+    <main className="p-6 container max-w-lg mx-auto">
+      <ul className="flex gap-4">
+        <li className="bg-white/50 hover:bg-white rounded-sm text-center">
+          <Link href="/gypsy">
+            <Image
+              src={gypsyImg}
+              width={68}
+              height={115}
+              alt="Gypsy class sprite"
+            />
+          </Link>
+        </li>
+      </ul>
     </main>
   );
 }
